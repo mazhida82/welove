49a094eb8fcabde79e8eacab14b000b60dad7572
mazhida <138766778@qq.com>
2017-12-25 17:04:15 +0800

    12/25 message back change  by mazhida

--- application/api/model/Message.php
+++ application/api/model/Message.php
@@ -28,9 +28,9 @@ class Message extends Base {
     public static function getInfo($data){
         $user_id = User::getUserIdByName($data['username']);
         $list = self::where(['user_id'=>$user_id,'id'=>$data['message_id'],'st'=>1])->find();
-//        if($list->isEmpty()){
-//            return ['code'=>__LINE__,'msg'=>'暂无数据'];
-//        }
+        if(!$list ->save(['type' => 1])){
+            return ['code'=>__LINE__,'msg'=>'修改状态失败'];
+        }
         return ['code'=>0,'data'=>$list];
     }
 
@@ -45,20 +45,4 @@ class Message extends Base {
         }
         return ['code' => 0, 'msg' => '删除成功'];
     }
-//    public function delMsg($id) {
-//        $row_ = self::where(['id' => $id,'st'=>1])->find();
-//        if (!$row_) {
-//            return ['code' => __LINE__, 'msg' => '留言不存在'];
-//        }
-//        if ($row_->st == 2) { //
-//            return ['code' => __LINE__, 'msg' => '留言已删除'];
-//        }
-//        $row_->st = 2;
-//        if (!$row_->save()) {
-//            return ['code' => __LINE__, 'msg' => '删除失败'];
-//        }
-//        return ['code' => 0, 'msg' => '删除成功'];
-//    }
-
-
 }
