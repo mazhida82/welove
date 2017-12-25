0050c056bd7eb337a99d9d1a38a5d75e5788158b
mazhida <138766778@qq.com>
2017-12-25 16:26:09 +0800

    12/25 message del api by mazhida

--- application/api/controller/MessageController.php
+++ application/api/controller/MessageController.php
@@ -37,49 +37,22 @@ class MessageController extends BaseController
         }
         return json(Message::getInfo($data));
     }
-//   /* /*
-//    *  select message from user and shop
-//    * */
-//    public function index(Request $request){
-//        $data = $request->get();
-//        $rule=[
-//            'username'=>'require','shop_id'=>'require'
-//        ];
-//        $res = $this->validate($data,$rule);
-//        if ($res !== true) {
-//            return json(['code' => __LINE__, 'msg' => $res]);
-//        }
-//        return json( Message::getList($data));
-//    }
-//    /*
-//     * add message from user
-//     * */
-//   public function save(Request $request){
-//       $data = $request->post();
-//       $rule=[
-//           'username'=>'require','shop_id'=>'require','message'=>'require'
-//       ];
-//       $res = $this->validate($data,$rule);
-//       if ($res !== true) {
-//           return json(['code' => __LINE__, 'msg' => $res]);
-//       }
-//       return json((new Message)->addMessage($data));
-//   }
-//    /*
-//* 用户删除留言
-//*
-//* zhuangxiu-zyg
-//* */
-//    public function del_by_user(Request $request) {
-//        $data=$request->post();
-//        $rule=[
-//            'msg_id'=>'require|number',
-//        ];
-//        $res = $this->validate($data,$rule);
-//        if ($res !== true) {
-//            return json(['code' => __LINE__, 'msg' => $res]);
-//        }
-//        return json((new Message)->delMsg($data['msg_id']));
-//    }*/
+
+    /**
+     * 用户删除消息
+     * @param Request $request
+     */
+    public function delete_user(Request $request){
+        $data = $request -> param();
+        $rule=[
+            'id'=>'require|number'
+        ];
+        $res = $this->validate($data, $rule);
+        if($res !== true){
+            return json(['code'=>__LINE__,'msg'=>'id传值错误']);
+        }
+        return json((new Message())->delMsgByUser($data['id']));
+    }
+
 
 }
--- application/api/model/Message.php
+++ application/api/model/Message.php
@@ -27,46 +27,24 @@ class Message extends Base {
      */
     public static function getInfo($data){
         $user_id = User::getUserIdByName($data['username']);
-        $list = self::where(['user_id'=>$user_id,'id'=>$data['message_id']])->find();
+        $list = self::where(['user_id'=>$user_id,'id'=>$data['message_id'],'st'=>1])->find();
 //        if($list->isEmpty()){
 //            return ['code'=>__LINE__,'msg'=>'暂无数据'];
 //        }
         return ['code'=>0,'data'=>$list];
     }
-//    /*
-//     * 用户留言列表
-//     *
-//     * zhuangxiu-zyg
-//     * */
-//    public static function getList($data) {
-//        $user_id = User::getUserIdByName($data['username']);
-//
-//        $list_ = self::where(['st' => 1, 'user_id' => $user_id, 'shop_id' => $data['shop_id']])->order('create_time asc')->select();
-//
-//        if ($list_->isEmpty()) {
-//            return ['code' => __LINE__];
-//        }
-//        return ['code' => 0, 'msg' => '数据成功', 'data' => $list_];
-//    }
-//
-//    /*
-// * 添加用户留言
-// *
-// * zhuangxiu-zyg
-// * */
-//    public function addMessage($data) {
-//        $user_id = User::getUserIdByName($data['username']);
-//        if (is_array($user_id)) {
-//            return $user_id;
-//        }
-//        $data['user_id'] = $user_id;
-//        unset($data['username']);
-//        if (!$this->save($data)) {
-//            return ['code' => __LINE__, 'msg' => '添加失败'];
-//        }
-//        return ['code' => 0, 'msg' => '添加成功'];
-//    }
-//
+
+    public function delMsgByUser($id){
+        $res = self::where(['id' => $id,'st' => 1])->find();
+        if(!$res){
+            return ['code' => __LINE__, 'msg' => '暂无此数据'];
+        }
+        $res->st = 2;
+        if (!$res->save()) {
+            return ['code' => __LINE__, 'msg' => '删除失败'];
+        }
+        return ['code' => 0, 'msg' => '删除成功'];
+    }
 //    public function delMsg($id) {
 //        $row_ = self::where(['id' => $id,'st'=>1])->find();
 //        if (!$row_) {
